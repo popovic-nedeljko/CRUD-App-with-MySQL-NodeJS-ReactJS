0b24cc354a0ef568bc8a02303bdd1f5a8b8b8adf
popovic-nedeljko <nftnedja@gmail.com>
2023-04-26 03:04:34 +0200

    added Book UI

--- client/src/pages/Books.jsx
+++ client/src/pages/Books.jsx
@@ -1,7 +1,39 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 const Books = () => {
-  return <div>bookss</div>;
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    const fetchAllBooks = async () => {
+      try {
+        const res = await axios.get('http://localhost:8800/books');
+        setBooks(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchAllBooks();
+  }, []);
+
+  return (
+    <div>
+      <h1>Your BOOKS</h1>
+      <div className='books'>
+        {books.map((book) => (
+          <div className='book' key={book.id}>
+            {book.cover && <img src={book.cover} alt='cover' />}
+            <h2>{book.title}</h2>
+            <p>{book.desc}</p>
+            <h3>{book.author}</h3>
+          </div>
+        ))}
+      </div>
+      <button>
+        <Link to='/add'>Add new book</Link>
+      </button>
+    </div>
+  );
 };
 
 export default Books;
